2fa6651e283f8af5e1806bd19ef7ee16431719c8
Don Griffin <dongryphon@users.noreply.github.com>
2017-05-12 14:45:45 -0500

    Update Package.js

--- packages/package-loader/src/Package.js
+++ packages/package-loader/src/Package.js
@@ -1,3 +1,9 @@
+/**
+ * This singleton provides a `load` method that is used to dynamically load packages.
+ * 
+ * This class leverages Sencha Cmd 6.5's package `uses` feature and the metadata it
+ * generates into the `Ext.manifest.packages` object.
+ */
 Ext.define('Ext.Package', {
     requires: [
         'Ext.package.Entry'
@@ -5,12 +11,69 @@ Ext.define('Ext.Package', {
 
     singleton: true,
 
+    /**
+     * Returns `true` if `pkgName` is already loaded.
+     *
+     * @param {String} pkgName The name of the package.
+     * @return {Boolean}
+     */
+    isLoaded: function (pkgName) {
+        var entry = this.getEntry(pkgName);
+        return entry ? entry.loaded : false;
+    },
+
+    /**
+     * Loads the specified package given its `pkgName`. Any packages required by
+     * the requested package will be loaded prior to the that package. In other
+     * words, the load is recursive.
+     *
+     * @param {String} pkgName The name of the package to load.
+     * @return {Promise}
+     */
+    load: function (pkgName) {
+        var me = this,
+            entry;
+        
+        if (!me.getPackages()[pkgName]) {
+            return Ext.Promise.reject(
+                new Error('Cannot load package "' + pkgName + '"'));
+        }
+
+        entry = me.getEntry(pkgName);
+        me._queue(entry);
+        if (!me._loading) {
+            me._advance();
+        }
+        
+        return entry.promise;
+    },
+    
+    //-----------------------------------------
+
     _entryMap: {},
     _usesMap: null,
     _packagesMap: null,
 
     _loadQueue: [],
     _loading: false,
+
+    _advance: function() {
+        var me = this,
+            queue = me._loadQueue,
+            next = queue.shift();
+        if (next) {
+            me._loading = true;
+            next.beginLoad();
+            next.promise.then(function () {
+                me._advance();
+            }, function () {
+                me._advance();
+            });
+        }
+        else {
+            me._loading = false;
+        }
+    },
     
     getPackages: function() {
         var map = this._packagesMap;
@@ -39,42 +102,6 @@ Ext.define('Ext.Package', {
         
         return entry;
     },
-    
-    load: function (pkgName) {
-        var me = this,
-            entry;
-        
-        if (!me.getPackages()[pkgName]) {
-            return Ext.Promise.reject(
-                new Error('Cannot load package "' + pkgName + '"'));
-        }
-
-        entry = me.getEntry(pkgName);
-        me._queue(entry);
-        if (!me._loading) {
-            me._advance();
-        }
-        
-        return entry.promise;
-    },
-
-    _advance: function() {
-        var me = this,
-            queue = me._loadQueue,
-            next = queue.shift();
-        if (next) {
-            me._loading = true;
-            next.beginLoad();
-            next.promise.then(function () {
-                me._advance();
-            }, function () {
-                me._advance();
-            });
-        }
-        else {
-            me._loading = false;
-        }
-    },
 
     loadAllScripts: function (packageName, scripts) {
         var me = this,
@@ -95,9 +122,5 @@ Ext.define('Ext.Package', {
         }
 
         return entry;
-    },
-
-    isLoaded: function (name) {
-        return this.getEntry(name).loaded;
     }
 });
